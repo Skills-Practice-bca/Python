5345467f9499531146692738b4153965cfd22f90
Kamal singh <161626814+kamalsingh27@users.noreply.github.com>
2024-09-25 22:44:20 +0530

    python work:

---
+++ question_2.py
@@ -0,0 +1,13 @@
+# to find the substring occurence in the string:
+
+string=input("string :")
+
+substring=input("substr :")
+
+
+
+# to find all occurences of the substring
+
+print(string.count(substring))
+
+
